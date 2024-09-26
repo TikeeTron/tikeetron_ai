5759cde74de2307ccfa0274b141e4d3a3b6dd949
Luthfi Arifin <luthfiarifin0222@gmail.com>
2024-09-27 01:20:41 +0700

    refactor: Update Dockerfile.development to run app with uvicorn and watch.py

--- app/events/watch.py
+++ app/events/watch.py
@@ -2,15 +2,15 @@ from bson.json_util import dumps
 
 from crud import insert_event_vector, update_event_vector, eventCollections
 
-
-change_stream = eventCollections.watch()
-for change in change_stream:
-    if change["operationType"] == "insert":
-        insert_event_vector(change["fullDocument"])
-    elif change["operationType"] == "update":
-        print(change["documentKey"]["_id"])
-        update_event_vector(
-            change["documentKey"]["_id"],
-            change["updateDescription"]["updatedFields"],
-        )
-    print(dumps(change))
+if __name__ == "__main__":
+    change_stream = eventCollections.watch()
+    for change in change_stream:
+        if change["operationType"] == "insert":
+            insert_event_vector(change["fullDocument"])
+        elif change["operationType"] == "update":
+            print(change["documentKey"]["_id"])
+            update_event_vector(
+                change["documentKey"]["_id"],
+                change["updateDescription"]["updatedFields"],
+            )
+        print(dumps(change))
