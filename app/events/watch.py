636774cb96755abfc39c0fc6e5885c9e760efb3e
Luthfi Arifin <luthfiarifin0222@gmail.com>
2024-10-01 19:22:40 +0700

    refactor: Update watch.py to use a while loop for continuous event watching and handling

--- app/events/watch.py
+++ app/events/watch.py
@@ -1,16 +1,24 @@
 from bson.json_util import dumps
-
 from crud import insert_event_vector, update_event_vector, eventCollections
 
+
+def watch_events():
+    while True:
+        try:
+            change_stream = eventCollections.watch()
+            for change in change_stream:
+                if change["operationType"] == "insert":
+                    insert_event_vector(change["fullDocument"])
+                elif change["operationType"] == "update":
+                    print(change["documentKey"]["_id"])
+                    update_event_vector(
+                        change["documentKey"]["_id"],
+                        change["updateDescription"]["updatedFields"],
+                    )
+                print(dumps(change))
+        except Exception as e:
+            print(f"Error occurred: {e}")
+
+
 if __name__ == "__main__":
-    change_stream = eventCollections.watch()
-    for change in change_stream:
-        if change["operationType"] == "insert":
-            insert_event_vector(change["fullDocument"])
-        elif change["operationType"] == "update":
-            print(change["documentKey"]["_id"])
-            update_event_vector(
-                change["documentKey"]["_id"],
-                change["updateDescription"]["updatedFields"],
-            )
-        print(dumps(change))
+    watch_events()
