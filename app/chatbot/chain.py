c643ffab93918bf5706d5ab591f3b415cceb9c75
Luthfi Arifin <luthfiarifin0222@gmail.com>
2024-10-02 01:43:13 +0700

    refactor: Update chatbot chain.py to improve event data retrieval and response formatting

--- app/chatbot/chain.py
+++ app/chatbot/chain.py
@@ -3,11 +3,16 @@ import os
 from dotenv import load_dotenv
 
 from langchain_groq import ChatGroq
-from langgraph.prebuilt import create_react_agent
+from langgraph.graph import StateGraph, END
+from langgraph.prebuilt import ToolNode
+from langchain_core.messages import HumanMessage
 
+
+from app.chatbot.model.response_model import AgentState, EventResponse, EventsResponse
 from app.chatbot.tools.get_tikeetron import get_tikeetron_tool
 from app.chatbot.tools.get_my_tickets import GetMyTicketsTool
 from app.chatbot.tools.get_current_events import get_current_event_tool
+from app.events.crud import get_event_datas
 
 load_dotenv()
 
@@ -22,24 +27,98 @@ llm = ChatGroq(
     temperature=0,
     max_tokens=None,
     timeout=None,
-    max_retries=2,
+    max_retries=1,
     api_key=os.getenv("GROQ_API_KEY"),
     verbose=True,
 )
 
-agent = create_react_agent(
-    llm,
-    tools,
+model_with_structured_output = llm.with_structured_output(EventsResponse)
+model_with_tools = llm.bind_tools(tools)
+
+
+# Define the function that calls the model
+def call_model(state: AgentState):
+    response = model_with_tools.invoke(state["messages"])
+    # We return a list, because this will get added to the existing list
+    return {"messages": [response]}
+
+
+# Define the function that responds to the user
+def respond(state: AgentState):
+    response = model_with_structured_output.invoke(
+        [HumanMessage(content=state["messages"][-2].content)]
+    )
+
+    if response.events and response.events[0].id:
+        return {
+            "events": response.events,
+            "message": "Here are the events I found:",
+        }
+    else:
+        return {
+            "events": [],
+            "message": "No events found.",
+        }
+
+
+# Define the function that determines whether to continue or not
+def should_continue(state: AgentState):
+    messages = state["messages"]
+    last_message = messages[-1]
+    # If there is no function call, then we respond to the user
+    if not last_message.tool_calls:
+        return "respond"
+    # Otherwise if there is, we continue
+    else:
+        return "continue"
+
+
+# Define a new graph
+workflow = StateGraph(AgentState)
+
+# Define the two nodes we will cycle between
+workflow.add_node("agent", call_model)
+workflow.add_node("respond", respond)
+workflow.add_node("tools", ToolNode(tools))
+
+# Set the entrypoint as `agent`
+# This means that this node is the first one called
+workflow.set_entry_point("agent")
+
+# We now add a conditional edge
+workflow.add_conditional_edges(
+    "agent",
+    should_continue,
+    {
+        "continue": "tools",
+        "respond": "respond",
+    },
 )
+
+workflow.add_edge("tools", "agent")
+workflow.add_edge("respond", END)
+graph = workflow.compile()
+
+
+def get_events(events: list):
+    return get_event_datas(events) if events else []
 
 
 def ask_agent(messages):
-    response = agent.invoke(
-        {
+    response = graph.invoke(
+        input={
             "messages": [
                 (
                     "system",
-                    "You are the Tikeetron Bot, designed to assist users with questions about events. Ensure that your responses are accurate and based on the provided passages.",
+                    """You are Tikeetron Bot. Follow these rules:
+1. For event-related questions (e.g., "What events are happening?"), call `get_events`, retrieve the event ID, name, and category, and return the event name along with a brief description. Ensure your response is based on the retrieved event data only.
+2. For ticket/transaction inquiries, call `get_my_tickets`.
+3. For general queries (NFTs or Tikeetron info), call `get_to_know_tikeetron`.
+
+Present the event data clearly in your response and do not ask for clarification unless absolutely necessary. Ensure all responses are based on the provided data without introducing any fabricated information.
+                    """.strip(
+                        "\n"
+                    ),
                 ),
                 (
                     "human",
@@ -47,10 +126,18 @@ def ask_agent(messages):
                 ),
             ],
         },
+        debug=True,
     )
+
+    print(response)
 
     message = response["messages"][-1]
     if message != None:
-        return message.content
+        return {
+            "message": message.content,
+            "events": get_events(response["events"]),
+        }
     else:
-        return "No response"
+        return {
+            "message": "I'm sorry, I don't have an answer for that.",
+        }
